7eb6ff88d1e505845beac34a0ecc4af20a6aa5af
Gaurav Garg <ggarg.bhilai@gmail.com>
2023-04-25 12:23:53 +0530

    Commit

---
+++ springboot-sqlserver/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+
+DROP TABLE IF EXISTS tutorials;
+
+create table tutorials
+( 
+   id INT IDENTITY(10001, 1) PRIMARY KEY,
+   title varchar(255) not null,
+   description varchar(255),
+   published INT
+);
