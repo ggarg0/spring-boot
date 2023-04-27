f4da5de6adedf36fe6930df121e80dd5a5522b8d
Gaurav Garg <ggarg.bhilai@gmail.com>
2023-04-27 16:20:27 +0530

    Commit

---
+++ springboot-actuator-with-logging/src/main/java/com/demo/app/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.demo.app.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.app.model.Greeting;
+
+@RestController
+public class DemoController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	private final Logger logger = LogManager.getLogger(this.getClass());
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public Greeting sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
+		logger.info("Fetching greeting details for " + name);
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
